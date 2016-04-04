ffc180eb69389d1f9722d50b330ee145e7617ee9
mkrawetko <mkrawetko@gmail.com>
2016-04-04 18:13:36 +0100

    refactored test to use junit Parameters

--- src/test/java/com/mkrawetko/ClimbStairsTest.java
+++ src/test/java/com/mkrawetko/ClimbStairsTest.java
@@ -1,23 +1,43 @@
 package com.mkrawetko;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class ClimbStairsTest {
 
     final ClimbStairs underTest = new ClimbStairs();
+    @Parameterized.Parameter
+    public int steps;
+    @Parameterized.Parameter(value = 1)
+    public int expected;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, 1},
+                {2, 2},
+                {3, 3},
+                {4, 5},
+                {5, 8},
+                {6, 13},
+                {7, 21},
+                {8, 34},
+        });
+    }
 
     @Test
     public void climbStairs() throws Exception {
-        assertThat(underTest.climbStairs(1), equalTo(1));
-        assertThat(underTest.climbStairs(2), equalTo(2));
-        assertThat(underTest.climbStairs(3), equalTo(3));
-        assertThat(underTest.climbStairs(4), equalTo(5));
-        assertThat(underTest.climbStairs(5), equalTo(8));
-        assertThat(underTest.climbStairs(6), equalTo(13));
-        assertThat(underTest.climbStairs(7), equalTo(21));
-        assertThat(underTest.climbStairs(8), equalTo(34));
+        int actual = underTest.climbStairs(steps);
+
+        assertThat(actual, equalTo(expected));
+
     }
 }
