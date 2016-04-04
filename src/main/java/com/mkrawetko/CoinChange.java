b819ca87250ba3ac4c71a523c45d7ea38f388502
mkrawetko <mkrawetko@gmail.com>
2016-04-04 18:08:07 +0100

    Implemented working CoinChange algorithms

--- src/main/java/com/mkrawetko/CoinChange.java
+++ src/main/java/com/mkrawetko/CoinChange.java
@@ -1,9 +1,47 @@
 package com.mkrawetko;
 
+import java.util.Arrays;
+
 public class CoinChange {
 
+    /**
+     * You are given coins of different denominations and a total amount of money amount.
+     * Write a function to compute the fewest number of coins that you need to make up that amount.
+     * If that amount of money cannot be made up by any combination of the coins, return -1.
+     * <p>
+     * Example 1:
+     * coins = [1, 2, 5], amount = 11
+     * return 3 (11 = 5 + 5 + 1)
+     * <p>
+     * Example 2:
+     * coins = [2], amount = 3
+     * return -1.
+     * <p>
+     * Note:
+     * You may assume that you have an infinite number of each kind of coin.
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
     public int coinChange(int[] coins, int amount) {
+        if (coins.length <= 0 || amount == 0) {
+            return amount == 0 ? 0 : -1;
+        }
+        int[] min = new int[amount + 1];
+        Arrays.sort(coins);
+        for (int i = coins[0]; i <= amount; i++) {
 
-        return 0;
+            for (int coin : coins) {
+                if (coin <= i) {
+                    int prevIdx = i - coin;
+                    if (prevIdx == 0 || (min[prevIdx] != 0 && (min[i] == 0 || min[prevIdx] + 1 < min[i]))) {
+                        min[i] = min[prevIdx] + 1;
+                    }
+                }
+            }
+        }
+        return min[amount] != 0 ? min[amount] : -1;
     }
+
 }
--- src/test/java/com/mkrawetko/CoinChangeTest.java
+++ src/test/java/com/mkrawetko/CoinChangeTest.java
@@ -1,11 +1,49 @@
 package com.mkrawetko;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
 public class CoinChangeTest {
+    @Parameter(value = 0)
+    public int[] coins;
+    @Parameter(value = 1)
+    public int amount;
+    @Parameter(value = 2)
+    public int expected;
+
+    private CoinChange underTest = new CoinChange();
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1}, 0, 0},
+                {new int[]{2}, 3, -1},
+                {new int[]{5}, 10, 2},
+                {new int[]{1, 2, 5}, 11, 3},
+                {new int[]{2, 5, 10, 1}, 27, 4},
+                {new int[]{2, 4, 5}, 13, 3},
+                {new int[]{3, 4, 7}, 17, 3},
+                {new int[]{186, 419, 83, 408}, 6249, 20}
+        });
+    }
 
     @Test
     public void coinChange() throws Exception {
 
+        final int actual = underTest.coinChange(coins, amount);
+
+        assertThat(actual, equalTo(expected));
     }
+
+
 }
