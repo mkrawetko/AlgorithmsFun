0087eb17461a136a067296ed35212a2f6727cac4
mkrawetko <mkrawetko@gmail.com>
2016-08-18 08:13:12 +0100

    added ZigZagConversion

---
+++ src/test/java/com/mkrawetko/ZigZagConversionTest.java
@@ -0,0 +1,40 @@
+package com.mkrawetko;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class ZigZagConversionTest {
+
+
+    @Parameterized.Parameter
+    public String s;
+    @Parameterized.Parameter(value = 1)
+    public int numsRows;
+    @Parameterized.Parameter(value = 2)
+    public String expected;
+    private ZigZagConversion underTest = new ZigZagConversion();
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"", 1, ""}
+                , {"PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"}
+        });
+    }
+
+    @Test
+    public void test() throws Exception {
+        String actual = underTest.convert(s, numsRows);
+
+        assertThat(actual, equalTo(expected));
+
+    }
+}
