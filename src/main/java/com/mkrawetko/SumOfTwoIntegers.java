cca6f786b14eefcd27e2f2ed320aad47176c9244
mkrawetko <mkrawetko@gmail.com>
2016-09-15 07:43:13 +0100

    added SumOfTwoIntegers problem

---
+++ src/main/java/com/mkrawetko/SumOfTwoIntegers.java
@@ -0,0 +1,14 @@
+package com.mkrawetko;
+
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * <p>
+ * Example:
+ * Given a = 1 and b = 2, return 3.
+ */
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        return 0;
+    }
+}
+
---
+++ src/test/java/com/mkrawetko/SumOfTwoIntegersTest.java
@@ -0,0 +1,39 @@
+package com.mkrawetko;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(Parameterized.class)
+public class SumOfTwoIntegersTest {
+    @Parameterized.Parameter(value = 0)
+    public int a;
+    @Parameterized.Parameter(value = 1)
+    public int b;
+    @Parameterized.Parameter(value = 2)
+    public int expected;
+
+    SumOfTwoIntegers underTest = new SumOfTwoIntegers();
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, 2, 3},
+        });
+    }
+
+    @Test
+    public void test() {
+
+        int actual = underTest.getSum(a, b);
+
+        assertThat(actual, equalTo(expected));
+    }
+}
