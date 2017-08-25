9151a974bd684b5e4f8bc116dfcbcfc128c0a9c7
mkrawetko <mkrawetko@gmail.com>
2017-08-25 07:34:40 +0100

    upgrade junit 5 to RC3 and use parametrized test in TwoSumTest

--- src/test/java/com/mkrawetko/TwoSumTest.java
+++ src/test/java/com/mkrawetko/TwoSumTest.java
@@ -1,29 +1,21 @@
 package com.mkrawetko;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Stream;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
-@RunWith(Parameterized.class)
-public class TwoSumTest {
+class TwoSumTest {
 
-
-    public static final int[] BIG_ARRAY = readBigArray();
-    @Parameterized.Parameter(value = 0)
-    public int[] nums;
-    @Parameterized.Parameter(value = 1)
-    public int target;
-    @Parameterized.Parameter(value = 2)
-    public int[] expected;
-    TwoSum underTest = new TwoSum();
+    private static final int[] BIG_ARRAY = readBigArray();
+    private TwoSum underTest = new TwoSum();
 
     private static int[] readBigArray() {
         try {
@@ -36,40 +28,28 @@ public class TwoSumTest {
         return new int[0];
     }
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                        {new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}}
-
-                        , {new int[]{11, 2, 15, 7}, 9, new int[]{1, 3}}
-
-                        , {new int[]{3, 2, 4}, 6, new int[]{1, 2}}
-
-                        , {new int[]{-1, -2, -3, -4, -5}, -8, new int[]{2, 4}}
-
-                        , {new int[]{-3, 4, 3, 90}, 0, new int[]{0, 2}}
-
-                        , {new int[]{0, 4, 3, 0}, 0, new int[]{0, 3}}
-
-                        , {BIG_ARRAY, 16021, new int[]{8010, 8011}}
-                }
+    private static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+                of(new int[]{11, 2, 15, 7}, 9, new int[]{1, 3}),
+                of(new int[]{3, 2, 4}, 6, new int[]{1, 2}),
+                of(new int[]{-1, -2, -3, -4, -5}, -8, new int[]{2, 4}),
+                of(new int[]{-3, 4, 3, 90}, 0, new int[]{0, 2}),
+                of(new int[]{0, 4, 3, 0}, 0, new int[]{0, 3}),
+                of(BIG_ARRAY, 16021, new int[]{8010, 8011})
         );
     }
 
 
-    @org.junit.Test
-    public void testTwoSumSortedArray() throws Exception {
-
-        final int[] actual = underTest.twoSumArraySorted(nums, target);
-
-        assertThat(actual, equalTo(expected));
+    @ParameterizedTest(name = "({0}, {1}) expected: {2}")
+    @MethodSource("argumentsProvider")
+    void testTwoSumSortedArray(int[] nums, int target, int[] expected) {
+        assertArrayEquals(expected, underTest.twoSumArraySorted(nums, target));
     }
 
-    @org.junit.Test
-    public void testTwoSumHashMap() throws Exception {
-
-        final int[] actual = underTest.twoSumHashMap(nums, target);
-
-        assertThat(actual, equalTo(expected));
+    @ParameterizedTest(name = "({0}, {1}) expected: {2}")
+    @MethodSource("argumentsProvider")
+    void testTwoSumHashMap(int[] nums, int target, int[] expected) {
+        assertArrayEquals(expected, underTest.twoSumHashMap(nums, target));
     }
 }
