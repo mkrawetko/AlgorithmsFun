14226f60b01a923da56c2b7c83ff541b6455e794
mkrawetko <mkrawetko@gmail.com>
2017-12-21 06:51:58 +0000

    Add reverse integer problem first implementation

--- src/main/nodejs/mkrawetko/ReverseInteger.js
+++ src/main/nodejs/mkrawetko/ReverseInteger.js
@@ -22,6 +22,33 @@
  * @return {number}
  */
 var reverse = function(x) {
+        if(x === 0 ){
+            return 0
+        }
        let str = x.toString()
-       while()
+       let res = ""
+       let idx = 0;
+       let addSign = false;
+       while(idx<str.length){
+           let c = str[idx]
+           if(c==='-'){
+               addSign = true
+           }else if(c!=='0' || parseInt(str.substr(idx)) !== 0){
+               res = c+res;
+           }
+           idx++
+       }
+
+        if(addSign){
+            res = '-'+res;
+        }
+       let ret = parseInt(res)
+       if(addSign && ret > 0){
+            return 0;
+       }else if(!addSign && ret<0){
+            return 0
+       }
+       return ret;
 };
+
+module.exports = reverse;
--- src/test/nodejs/mkrawetko/ReverseInteger.spec.js
+++ src/test/nodejs/mkrawetko/ReverseInteger.spec.js
@@ -15,5 +15,14 @@ describe('#reverseInteger(x,y) ', function () {
         it('on input 120 should return 21', function () {
             expect(reverse(120)).to.equal(21)
         });
+        it('on input 0 should return 0', function () {
+            expect(reverse(0)).to.equal(0)
+        });
+        it('on input 901000 should return 109', function () {
+            expect(reverse(901000)).to.equal(109)
+        });
+        it('on input 1534236469 should return 0 overflows', function () {
+            expect(reverse(1534236469)).to.equal(0)
+        });
 
 });
