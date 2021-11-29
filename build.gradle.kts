748e88d22da8563a1ff02ff2dcf8b95bd3611334
mkrawetko <mkrawetko@gmail.com>
2021-11-29 09:06:40 +0000

    Add node plugin to run js implementation and tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm") version "1.6.0"
+	id ("com.github.node-gradle.node") version "3.1.1"
 }
 
 repositories {
@@ -9,9 +10,9 @@ repositories {
 }
 
 dependencies {
-	testImplementation(platform("org.junit:junit-bom:5.8.1"))
+	testImplementation(platform("org.junit:junit-bom:5.8.2"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
-	testCompileOnly("junit:junit:4.13")
+	testCompileOnly("junit:junit:4.13.2")
 	testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
 
@@ -25,4 +26,16 @@ tasks.test {
 // config JVM target to 1.8 for kotlin compilation tasks
 tasks.withType<KotlinCompile>().configureEach {
 	kotlinOptions.jvmTarget = "1.8"
+}
+
+node {
+	// Whether to download and install a specific Node.js version or not
+	// If false, it will use the globally installed Node.js
+	// If true, it will download node using above parameters
+	// Note that npm is bundled with Node.js
+	download.set(true)
+}
+
+task("testAll"){
+	dependsOn("check","npm_test")
 }
