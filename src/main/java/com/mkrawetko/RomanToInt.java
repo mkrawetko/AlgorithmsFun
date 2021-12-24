242aad81455b91d2ccce3e5a50fb61fd3d6fb040
mkrawetko <mkrawetko@gmail.com>
2021-12-24 11:27:15 +0000

    Refresh RomanToInt solution use junit5

--- src/main/java/com/mkrawetko/RomanToInt.java
+++ src/main/java/com/mkrawetko/RomanToInt.java
@@ -2,39 +2,133 @@ package com.mkrawetko;
 
 
 /**
+ * Easy
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * <p>
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
  * Given a roman numeral, convert it to an integer.
- * Input is guaranteed to be within the range from 1 to 3999.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ * Example 2:
+ * <p>
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ * Example 3:
+ * <p>
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 15
+ * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
  */
 public class RomanToInt {
 
-    static short h[] = new short[22];
-
-    static {
-        h[6] = 1;
-        h[19] = 5;
-        h[21] = 10;
-        h[9] = 50;
-        h[0] = 100;
-        h[1] = 500;
-        h[10] = 1000;
-    }
-
     public int romanToInt(String romanStr) {
-
-        int sum = 0;
-        int currentIdx = 0;
+        int result = 0;
         for (int i = 0; i < romanStr.length(); i++) {
-            short cur = h[romanStr.charAt(i) - 67];
-            short next = (i + 1) < romanStr.length() ? h[romanStr.charAt(i + 1) - 67] : -1;
-            if (cur < next) {
-                sum += next - cur;
+            RomanNumber curr = new RomanNumber(romanStr.charAt(i));
+            RomanNumber next = i < romanStr.length() - 1 ? new RomanNumber(romanStr.charAt(i + 1)) : null;
+            if (next != null && curr.intValue < next.intValue) {
+                result += next.intValue - curr.intValue;
                 i++;
             } else {
-                sum += cur;
+                result += curr.intValue;
             }
         }
+        return result;
+    }
 
-        return sum;
+    public int romanToInt_Backwards(String romanStr) {
+        int result = 0;
+        RomanNumber prev = new RomanNumber(romanStr.charAt(romanStr.length() - 1));
+        result += prev.intValue;
+        for (int i = romanStr.length() - 2; i >= 0; i--) {
+            RomanNumber curr = new RomanNumber(romanStr.charAt(i));
+            if (curr.intValue < prev.intValue) {
+                result -= curr.intValue;
+            } else {
+                result += curr.intValue;
+            }
+            prev = curr;
+        }
+        return result;
+    }
+
+    public int romanToInt_Recursive(String romanStr) {
+        return new RomanNumber(romanStr.charAt(romanStr.length() - 1)).intValue + romanToIntRecursiveInternal(romanStr.length() - 2, romanStr);
+    }
+
+    private int romanToIntRecursiveInternal(int i, String romanStr) {
+        if (i < 0) {
+            return 0;
+        } else {
+            RomanNumber curr = new RomanNumber(romanStr.charAt(i));
+            int currVal = curr.intValue;
+            if (new RomanNumber(romanStr.charAt(i + 1)).intValue > curr.intValue) {
+                currVal *= -1;
+            }
+            return currVal + romanToIntRecursiveInternal(--i, romanStr);
+        }
+    }
+
+    static class RomanNumber {
+        public final char roman;
+        public final int intValue;
+
+        public RomanNumber(char roman) {
+            this.roman = roman;
+            switch (roman) {
+                case 'I':
+                    this.intValue = 1;
+                    break;
+                case 'V':
+                    this.intValue = 5;
+                    break;
+                case 'X':
+                    this.intValue = 10;
+                    break;
+                case 'L':
+                    this.intValue = 50;
+                    break;
+                case 'C':
+                    this.intValue = 100;
+                    break;
+                case 'D':
+                    this.intValue = 500;
+                    break;
+                case 'M':
+                    this.intValue = 1000;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid Roman Sign: " + roman);
+            }
+        }
     }
 
 }
--- src/test/java/com/mkrawetko/RomanToIntTest.java
+++ src/test/java/com/mkrawetko/RomanToIntTest.java
@@ -1,38 +1,52 @@
 package com.mkrawetko;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Stream;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
-@RunWith(Parameterized.class)
 public class RomanToIntTest {
-
-    @Parameterized.Parameter(value = 0)
-    public String romanStr;
-    @Parameterized.Parameter(value = 1)
-    public int expected;
 
     RomanToInt underTest = new RomanToInt();
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {"IV", 4},
-                {"DCXXI", 621},
-        });
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                of("V", 5),
+                of("II", 2),
+                of("LVI", 56),
+                of("IV", 4),
+                of("III", 3),
+                of("LVIII", 58),
+                of("MCMXCIV", 1994),
+                of("DCXXI", 621)
+        );
     }
 
-    @Test
-    public void test() {
-
+    @ParameterizedTest
+    @MethodSource("data")
+    public void shouldConvertRomanToInt(String romanStr, int expected) {
         int actual = underTest.romanToInt(romanStr);
 
-        assertThat(actual, equalTo(expected));
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void shouldConvertRomanToInt_backwards(String romanStr, int expected) {
+        int actual = underTest.romanToInt_Backwards(romanStr);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void shouldConvertRomanToInt_recursive(String romanStr, int expected) {
+        int actual = underTest.romanToInt_Recursive(romanStr);
+
+        assertEquals(expected, actual);
     }
 }
